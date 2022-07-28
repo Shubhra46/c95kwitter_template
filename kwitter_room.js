be4b5683b13c60bb02b297885b994e0c1e55397d
Shubhra46 <73549763+Shubhra46@users.noreply.github.com>
2022-07-28 19:20:49 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,16 @@
+
+//ADD YOUR FIREBASE LINKS
+
+  
+
+function getData() {  firebase.database().ref("/").on('value', function(snapshot) { document.getElementById("output").innerHTML = ""; snapshot.forEach(function(childSnapshot) { childKey  = childSnapshot.key;
+       Room_names = childKey;
+       
+    });
+  });
+
+}
+
+getData();
+
+
